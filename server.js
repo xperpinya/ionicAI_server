717811ac898d46a8ced315c5a92c0193ca0318b9
xperpinya <xperpinya@gmail.com>
2018-03-14 13:06:51 +0100

    get-archive test

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.post('/', function(req, res){
 		// })
 
 	} else if (req.body.result.action == "Get-archive"){
-		var pdfAktios = app.use(express.static(path.join(__dirname,"archives/Corporativo.pdf")));
-		// var pdfAktios = "https://www.aktiosdigitalservices.com/assets/Aktios_Digital_Services.pdf";
+		// var pdfAktios = app.use(express.static(path.join(__dirname,"archives/Corporativo.pdf")));
+		var pdfAktios = "https://www.aktiosdigitalservices.com/assets/Aktios_Digital_Services.pdf";
 		// res.json({"url": pdfAktios})s
 		var response = "Este el pedido que me has solicitado";
 		res.json({
